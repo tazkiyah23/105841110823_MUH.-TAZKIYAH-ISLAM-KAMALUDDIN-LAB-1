6c63854ace55e22f5c2d7d6d7cf850a9f97add22
tazkiyah23 <157344090+tazkiyah23@users.noreply.github.com>
2024-06-03 12:07:23 +0800

    TUGAS 2

---
+++ zk.js
@@ -0,0 +1,6 @@
+function myFunction() {
+	document.getElementById("*").innerHTML = "MUH. TAZKIYAH ISLAM KAMALUDDIN";
+    document.getElementById("1").innerHTML = "20";
+	document.getElementById("2").innerHTML = "40";
+	document.getElementById("3").innerHTML = "60";
+}
